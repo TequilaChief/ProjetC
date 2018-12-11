871465ef3069585af37621687f7783a8032c3cef
Emric <emric.pichonnier@gmail.com>
2018-12-11 10:32:18 +0100

    découpage et sélection de syllabe

---
+++ librairy/select-syllable.h
@@ -0,0 +1,11 @@
+char* lastSyllabe(char str[],int i){
+    char *pch;
+    char *last_syllable;
+    pch = strtok (str,"-");
+    last_syllable=pch;
+    for(int e=0;e<i;e++){
+        last_syllable=pch;
+        pch = strtok (NULL, "-");
+    }
+    return last_syllable;
+}
