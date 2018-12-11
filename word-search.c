9e4fc953ea3c6ae6ab6028df7f5ca2a290b5a649
Choura <matsim@orange.fr>
2018-12-11 11:36:29 +0100

    edit

--- word-search.c
+++ word-search.c
@@ -5,14 +5,23 @@
  {
  char *location, buf2[80], buf1[] = "alex bob charlie david frank";
  /* Input the strings. */
+
+
  printf("Enter the target string: ");
  gets(buf2);
  /* Perform the search. */
+
+
+
+
  location = strstr(buf1, buf2);
+ printf("%s",strstr(buf1, buf2));
+
+
  if ( location == NULL )
  printf("No match was found.\n");
  else
  printf("%s was found at position %d.\n", buf2, location-buf1);
  return(0);
-     
+
  }
